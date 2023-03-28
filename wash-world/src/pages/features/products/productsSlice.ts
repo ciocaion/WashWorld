d19db22e4f73b219d714ca12c6660197a4ab3b2f
Ion Cioca <ciocaion2298@gmail.com>
2023-03-28 10:53:46 +0200

    added comments on the store & slice code

--- wash-world/src/pages/features/products/productsSlice.ts
+++ wash-world/src/pages/features/products/productsSlice.ts
@@ -1,6 +1,8 @@
+// Import the createSlice function from Redux Toolkit and the RootState type from the store file
 import { createSlice } from '@reduxjs/toolkit';
 import type { RootState } from './store';
 
+// Define the shape of the Product interface
 interface Product {
   id: number;
   name: string;
@@ -10,26 +12,34 @@ interface Product {
   estimated_duration: string;
 }
 
+// Define the shape of the ProductState interface
 interface ProductState {
   selectedProduct: Product | null;
 }
 
+// Set the initial state of the product slice
 const initialState: ProductState = {
   selectedProduct: null,
 };
 
+// Create a slice of the Redux store called 'product'
 const productSlice = createSlice({
   name: 'product',
   initialState,
   reducers: {
+    // Add a reducer function called 'setSelectedProduct' to the slice
     setSelectedProduct: (state, action) => {
+      // Update the state of the selectedProduct field with the payload of the action
       state.selectedProduct = action.payload;
     },
   },
 });
 
+// Export the setSelectedProduct reducer action for use in other parts of the application
 export const { setSelectedProduct } = productSlice.actions;
 
+// Export a selector function that retrieves the selectedProduct field from the Redux store's 'product' slice
 export const selectSelectedProduct = (state: RootState) => state.product.selectedProduct;
 
+// Export the product slice reducer as the default export of this file
 export default productSlice.reducer;
--- wash-world/src/pages/store.ts
+++ wash-world/src/pages/store.ts
@@ -1,13 +1,19 @@
+// Import the configureStore function from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+// Import the productReducer from the productsSlice file
 import productReducer from './features/products/productsSlice';
 
+// Configure the Redux store using configureStore
 const store = configureStore({
   reducer: {
+    // Pass the productReducer as the reducer for the 'product' slice of the store
     product: productReducer,
   },
 });
 
+// Define two types that can be used throughout the application: RootState and AppDispatch
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+// Export the store as the default export of this file
 export default store;
